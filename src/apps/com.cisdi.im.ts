0a2ceddd08307111568ac1448f99f06d376ced35
Gallen <gallenhu@outlook.com>
2024-03-26 08:40:37 +0800

    Create com.cisdi.im.ts
    Signed-off-by: Gallen <gallenhu@outlook.com>

---
+++ src/apps/com.cisdi.im.ts
@@ -0,0 +1,21 @@
+import { defineAppConfig } from '../types';
+
+export default defineAppConfig({
+  id: 'com.cisdi.im',
+  name: '轻推',
+  groups: [
+    {
+      key: 1,
+      name: '更新提醒',
+      activityIds: ['im.qingtui.app.ui.activity.MainActivity'],
+      rules: [
+        {
+          matches: 'LinearLayout > @TextView[text=`稍后`]',
+          snapshotUrls: [
+            'https://i.gkd.li/import/14659011',
+          ],
+        },
+      ],
+    },
+  ],
+});
